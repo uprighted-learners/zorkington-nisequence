5b96dbec945cae0192a0b6c127a3a28c56336b65
nisequence <nicolesekol@gmail.com>
2023-03-04 14:57:04 -0500

    Created state machines and two functions, began testing for user input

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ class Location {
     this.description = desc;
   }
   
-  // look function returns location description
+  // look method returns location description
   look() {
-    console.log(this.description);
+    console.log(this.description + "\n");
   }
 }
 
@@ -30,7 +30,7 @@ class Room extends Location {
     this.treasure = within;
   }
 
-  // search function
+  // search method
   search(param) {
     let newParam = param.toLowerCase();
     if (newParam == this.thing) {
@@ -50,7 +50,7 @@ function listInventory() {
   console.log(`You open your backpack and see ${inventory}.`);
 }
 
-//*List of Rooms:
+//* List of Rooms:
 roomList = [];
 
 let library = new Room("Library", "The library is a room with three doors, otherwise filled with floor-to-ceiling bookshelves, reading nooks, and a coffee table.", "bookshelves", "a golden book");
@@ -80,7 +80,7 @@ roomList.push(conservatory);
 let billiardRoom = new Room("Billiard Room", "The Billiard Room is blah blah blah", "thing", "treasure");
 roomList.push(billiardRoom);
 
-console.log(roomList); // end of Rooms
+// end of Rooms
 
 //* List of Locations
 locations = [];
@@ -91,13 +91,119 @@ locations.push(frontYard);
 let westernHall = new Location("Western Hall", "The Western Hall is a short corridor outside the Billiard Room. It appears that there are two other doors on its southern side: one to the west, and one to the east.");
 locations.push(westernHall);
 
-let easternHall = new Location("Eastern Hall", "The Eastern Hall is an L-shaped corridor with four cedar doors: three are adorned with crystal knobs and boast intricate edging. The southernmost door is plain and unassuming.");
+let easternHall = new Location("Eastern Hall", "The Eastern Hall is an L-shaped corridor with four cedar doors: three are adorned with crystal knobs and boast intricate edging. The southernmost door, tucked around the corner, is plain and unassuming.");
 locations.push(easternHall);
 
-let backyard = new Location("Back Yard", "The Back Yard appears to have been the host of a recent croquet match. A set of polished croquet balls sits alone on the finely-decorated patio")
+let backyard = new Location("Back Yard", "The Back Yard appears to have been the host of a recent croquet match. A set of polished croquet balls sits alone on the finely-decorated patio.")
 locations.push(backyard);
 
-console.log(locations);
+let currentLocation = frontYard;
+let roomCall = [];
+// end of Locations
+
+//* State Machines
+const reverseCall = {
+  "fron": frontYard,
+  "main": mainHall,
+  "libr": library,
+  "stud": study,
+  "bill": billiardRoom,
+  "west": westernHall,
+  "cons": conservatory,
+  "back": backyard,
+  "kitc": kitchen,
+  "east": easternHall,
+  "ball": ballRoom,
+  "dini": diningRoom,
+  "loun": lounge,
+};
+
+/* delete?
+const roomCalling = {
+  frontYard: ["Front Yard", "front yard", "front Yard", "Front yard"],
+  mainHall: ["Main Hall", "main hall", "Main hall", "main Hall"],
+  library: ["Library", "library"],
+  study: ["study", "Study"],
+  billiardRoom: ["Billiard Room", "billiard Room", "Billiard room", "billiard room"],
+  westernHall: ["Western Hall", "western hall", "Western hall", "western Hall"],
+  conservatory: ["conservatory", "Conservatory"],
+  backyard: ["backyard", "Backyard", "Back Yard", "back yard", "Back yard", "back Yard"],
+  kitchen: ["kitchen", "Kitchen"],
+  easternHall: ["eastern Hall", "Eastern hall", "eastern hall", "Eastern Hall"],
+  ballRoom: ["Ballroom", "ballroom", "Ball Room", "ball room", "Ball room", "ball Room"],
+  diningRoom: ["Dining Room", "dining room", "Dining room", "dining Room"],
+  lounge: ["Lounge", "lounge"]
+};
+
+*/
+
+let accessible2 = {
+  frontYard: ["Main Hall"],
+  mainHall: ["Front Yard", "Library", "Ballroom"],
+  library: ["Main Hall", "Study", "Billiard Room"],
+  study: ["Library"],
+  billiardRoom: ["Ballroom", "Western Hall"],
+  westernHall: ["Billiard Room", "Back Yard"], // "Conservatory" (locked)
+  conservatory: ["Western Hall"],
+  backyard: ["Western Hall", "Kitchen"],
+  kitchen: ["Back Yard", "Eastern Hall"],
+  easternHall: ["Kitchen", "Dining Room", "Ball Room", "Lounge"],
+  ballRoom: ["Main Hall", "Billiard Room", "Eastern Hall"],
+  diningRoom: ["Eastern Hall"],
+  lounge: ["Eastern Hall"],
+}
+
+//* Read Room Answer Function
+function convertRoomResponse(inputRoom) {
+  let convertResponse = inputRoom.substring(0, 4);
+  console.log(convertResponse);
+  let lowercaseConversion = convertResponse.toLowerCase();
+  let seekOption = reverseCall[lowercaseConversion];
+  if (seekOption == undefined) {
+    console.log(`Sorry, I don't know what ${inputRoom} is.`);
+  } else {
+    console.log(`Attempting to switch rooms to ${seekOption.name}.`);
+    changeRoom(seekOption.name);
+  }
+}
+
+//* Room Changing Function
+function changeRoom(newRoom) {
+  // check valid rooms based on current location
+  let validMovement = accessible2[currentLocation];
+  console.log(validMovement);
+  // announce current location
+  console.log(`You are currently in the ${currentLocation.name}.`);
+
+  // if else to determine what to do if newRoom is accessible and what to do if not
+  if (validMovement.includes(newRoom)) {
+    //allow movement
+    let currentLocation = newRoom;
+    console.log(`You move into the ${currentLocation.name}.`);
+  } else {
+    console.log(`Invalid movement: ${currentLocation.name} to ${newRoom}. Try something else.`);
+  }
+
+}
+
+
+//! Test Section ***************************
+//? Game starts in frontYard
+//console.log(roomList);
+//console.log(locations);
+//console.log(easternHall.description);
+//console.log(currentLocation.description);
+console.log(currentLocation.look());
+convertRoomResponse("Main Hall");
+//!changeRoom(mainHall);
+console.log(currentLocation.look());
+
+//!let convertResponse = playerResponse.split(" ");
+//!console.log(convertResponse);
+//!let adjustResponse = convertResponse[1].toLowerCase;
+//!console.log(adjustResponse);
+//!let newResponse = convertResponse[0].toUpperCase;
+//!console.log(newResponse);
 
 //!start();
 
